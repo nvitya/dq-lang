786008d7ad3e9ad832cfd46cdd441d15048b434b
Viktor Nagy <nvitya@gmail.com>
2026-05-22 21:17:23 +0200

    autotest cleanup script

---
+++ autotest/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+TEST_DIR="$SCRIPT_DIR/tests"
+
+find "$TEST_DIR" -type d -name .dqbuild -prune -exec rm -rf {} +
+find "$TEST_DIR" -type f \( \
+  -name '*.exe' -o \
+  -name '*.o' -o \
+  -name '*.dqm' -o \
+  -name '*.dqm_if' -o \
+  -name '*.atr' \
+\) -exec rm -f {} +

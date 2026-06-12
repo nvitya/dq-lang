e2da1935aeba1e0605fccf39be1629c30349d4a1
nvitya <nvitya@gmail.com>
2026-06-12 13:31:57 +0200

    updated Rust test

--- examples/time/test_dynarr_rs.rs
+++ examples/time/test_dynarr_rs.rs
@@ -4,36 +4,43 @@ use std::hint::black_box;
 
 const DEFAULT_MAXVAL: i32 = 1000000;
 
-fn fill_array(darr: &mut Vec<i32>, maxval: i32) {
-    darr.clear();
-    for i in 0..maxval {
-        darr.push(black_box(i));
+#[allow(non_upper_case_globals)]
+static mut darr: Vec<i32> = Vec::new();
+
+fn fill_array(maxval: i32) {
+    unsafe {
+        darr.clear();
+        for i in 0..maxval {
+            darr.push(black_box(i));
+        }
     }
 }
 
-fn fill_array_ptr(darr: &mut Vec<i32>, maxval: i32) {
-    darr.resize(maxval as usize, 0);
-    let ptr = darr.as_mut_ptr();
-    for i in 0..maxval {
-        unsafe {
+fn fill_array_ptr(maxval: i32) {
+    unsafe {
+        darr.resize(maxval as usize, 0);
+        let ptr = darr.as_mut_ptr();
+        for i in 0..maxval {
             *ptr.add(i as usize) = i;
         }
     }
 }
 
-fn calc_sum(darr: &Vec<i32>) -> i64 {
+fn calc_sum() -> i64 {
     let mut result: i64 = 0;
-    for i in 0..darr.len() {
-        result += black_box(darr[i] as i64);
+    unsafe {
+        for i in 0..darr.len() {
+            result += black_box(darr[i] as i64);
+        }
     }
     result
 }
 
-fn calc_sum_ptr(darr: &Vec<i32>) -> i64 {
+fn calc_sum_ptr() -> i64 {
     let mut result: i64 = 0;
-    let ptr = darr.as_ptr();
-    for i in 0..darr.len() {
-        unsafe {
+    unsafe {
+        let ptr = darr.as_ptr();
+        for i in 0..darr.len() {
             result += black_box(*ptr.add(i) as i64);
         }
     }
@@ -54,17 +61,15 @@ fn main() {
 
     println!("maxval = {}", maxval);
 
-    let mut darr: Vec<i32> = Vec::new();
-
     println!("Filling the dynamic array...");
     let start1 = Instant::now();
-    fill_array(&mut darr, maxval);
+    fill_array(maxval);
     let elapsed1 = start1.elapsed();
     println!("Total fill time: {} us", elapsed1.as_micros());
 
     println!("Summing the dynamic array...");
     let start2 = Instant::now();
-    let sum = calc_sum(&darr);
+    let sum = calc_sum();
     let elapsed2 = start2.elapsed();
     println!("sum = {}", sum);
     println!("Total sum time: {} us", elapsed2.as_micros());
@@ -73,13 +78,13 @@ fn main() {
 
     println!("Filling the dynamic array (ptr)...");
     let start3 = Instant::now();
-    fill_array_ptr(&mut darr, maxval);
+    fill_array_ptr(maxval);
     let elapsed3 = start3.elapsed();
     println!("Total fill time: {} us", elapsed3.as_micros());
 
     println!("Summing the dynamic array (ptr)...");
     let start4 = Instant::now();
-    let sum_ptr = calc_sum_ptr(&darr);
+    let sum_ptr = calc_sum_ptr();
     let elapsed4 = start4.elapsed();
     println!("sum = {}", sum_ptr);
     println!("Total sum time: {} us", elapsed4.as_micros());

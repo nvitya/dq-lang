c97d960f851524b6c863d7548aec36fa8ee98b1e
Viktor Nagy <nvitya@gmail.com>
2026-06-20 08:46:22 +0200

    v0.20.18:   - Backtrace formatting changes

--- compiler/src/version.h
+++ compiler/src/version.h
@@ -11,10 +11,12 @@
  * brief:   DQ Compiler Version Description
  */
 
-#define DQ_COMPILER_VERSION  "0.20.17"
+#define DQ_COMPILER_VERSION  "0.20.18"
 
 /* CHANGE LOG
 ------------------------------------------------------------------------------------
+v0.20.18:
+  - Backtrace formatting changes
 v0.20.17:
   - Using libbacktrace instead of addr2line
 v0.20.16:
